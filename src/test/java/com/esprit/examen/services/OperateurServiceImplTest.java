4c1fc76e0cff87dc3f357f37bd83458dc18a46eb
Eya Yousfi <48035754+Eyaaaa@users.noreply.github.com>
2022-11-17 21:13:29 -1100

    Update OperateurServiceImplTest.java

--- src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -19,32 +19,16 @@ public class OperateurServiceImplTest {
 
     @Test
     public void testAddOperateur() {
-        //	List<Stock> stocks = stockService.retrieveAllStocks();
-        //	int expected=stocks.size();
+  
         Operateur s = new Operateur("test","test","test");
         Operateur savedOperateur= operateurService.addOperateur(s);
 
-        //	assertEquals(expected+1, stockService.retrieveAllStocks().size());
+       
         assertNotNull(savedOperateur.getNom());
          operateurService.deleteOperateur(savedOperateur.getIdOperateur());
 
     }
 
-    /*@Test
-    public void testAddStockOptimized() {
-        Stock s = new Stock("stock test",10,100);
-        Stock savedStock= stockService.addStock(s);
-        assertNotNull(savedStock.getIdStock());
-        assertSame(10, savedStock.getQte());
-        assertTrue(savedStock.getQteMin()>0);
-        stockService.deleteStock(savedStock.getIdStock());
-    }
-    @Test
-    public void testDeleteStock() {
-        Stock s = new Stock("stock test",30,60);
-        Stock savedStock= stockService.addStock(s);
-        stockService.deleteStock(savedStock.getIdStock());
-        assertNull(stockService.retrieveStock(savedStock.getIdStock()));
-    }*/
+    
 
 }
